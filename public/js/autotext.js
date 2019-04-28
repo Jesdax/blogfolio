17e9d0299a713a750131497fc604e05a5440b37f
Jesdax <augustin_kave_08@outlook.fr>
2019-04-28 12:14:50 +0200

    Edit autotexte js

--- public/js/autotext.js
+++ public/js/autotext.js
@@ -492,7 +492,7 @@ function handleEscKey() {
 
 function handleTyping () {
     $(".element").typed({
-        strings: ["chef de projet digital junior", "développeur web junior",  "motivé à 300% !", "autodidacte / autonome"],
+        strings: ["autonome", "rigoureux",  "motivé à 300% !", "votre futur maestro", "votre prochain développeur web"],
         typeSpeed: 50,
         starDelay: 200,
         backDelay: 800,
